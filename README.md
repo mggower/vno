ba49d05ef34b53c0373824549c6c5ca5a7c0a789
mggower <gowermikey@gmail.com>
2020-12-10 14:15:21 -0500

    adding work code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vno
+
+_once upon a time in a land far far away..._
---
+++ src/test.js
@@ -0,0 +1,52 @@
+console.log('before');
+/*
+reading file 'readTextFile' allows us to read and write in plain english
+"readFile --> gives us UNICODE Characters" X X X
+*/
+const data = await Deno.readTextFile('./please.vue');
+
+const tRegex = /<\W*template>/gm;
+const template = data.split(tRegex)[1].split(/\n/).join('');
+// template.replace(/n/gm, '');
+// template.trim();
+// const pls = template;
+// console.log(pls);
+console.log('template ->', template);
+console.log('typeof template ->', typeof template);
+
+const scrRegex = /<\W*script>/gm;
+const script = data.split(scrRegex)[1];
+
+console.log('script ->', script);
+console.log('typeof script ->', typeof script);
+
+const styRegex = /<\W*style>/gm;
+const style = data.split(styRegex)[1];
+
+console.log('style ->', style);
+console.log('typeof style ->', typeof style);
+
+const object = {
+  template,
+  script,
+  style,
+};
+console.log('object', object);
+
+/* mkdir and write to new file
+
+make directory if 'vno-build' does not exits
+this is where all of our build files will go
+*/
+const app = 'thisBadBoy';
+// we'll use syntax like this to populate our files with the correct syntax
+const output = `const ${app}  = Vue.component('${app}', {
+  template: "${template}",
+  ${script}
+})`;
+/*
+copy and write content from .Vue file into a new doc
+*/
+// await Deno.mkdir('vno-build');
+await Deno.writeTextFile('./vno-build/Output.js', output, { create: true });
+// console.log('File data:', data);
