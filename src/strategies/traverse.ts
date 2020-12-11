395ea48c69ba073f1a8cc0bfa7629b448309544b
mggower <gowermikey@gmail.com>
2020-12-11 13:35:07 -0500

    attempts to refactor

--- src/strategies/traverse.ts
+++ src/strategies/traverse.ts
@@ -1,36 +1,58 @@
-const path: string = "./App.vue";
+import { join } from "https://deno.land/std@0.74.0/path/mod.ts";
+
+const rootPath: string = "./App.vue";
+const findPath = (relative: string) => join(Deno.cwd(), `.${relative}`);
 // queue is components to be traversed
 const queue: Array<object> = [];
 // cache holds components after they have been parsed
 const cache: Array<object> = [];
 
-const traverse = async (pathname: string) => {
-  const root = await Deno.readTextFile(pathname);
-  // console.log("root -->", root);
+interface component {
+  name: string;
+  path: string;
+}
 
-  const lines = root.split(/\n/);
+const parseImports = (data: string) => {
+  const lines = data.split(/\n/);
   // console.log("lines --> ", lines);
 
   const regex = /^(import)/;
   const imports = lines.filter((n) => regex.test(n));
-
   // console.log("imports --> ", imports);
+
+  return imports;
+};
+
+const traverse = async (component: component) => {
+  console.log(component, "component");
+  queue.push(component);
+
+  const { path } = component;
+  const root = await Deno.readTextFile(path);
+  // console.log("root -->", root);
+  
+  const imports = parseImports(root);
 
   imports.forEach((item) => {
     const arr = item.split(" ");
     const el = arr[1];
-    const path = arr[3].split("'")[1];
+    const path = findPath(arr[3].split("'")[1]);
     const component = { el, path };
     queue.push(component);
   });
 
-  while (queue.length) {
-    const current: any = queue.shift();
-    console.log(current);
-    traverse(current.path);
-    cache.push(current);
-  }
-  // console.log(cache, "cache");
+  // while (queue.length) {
+  //   const current: any = queue.shift();
+  //   console.log(current);
+  //   traverse(current.path);
+  //   cache.push(current);
+  // }
+  console.log(cache, "cache");
 };
 
-traverse(path);
+const root = {
+  name: "App",
+  path: findPath(rootPath),
+};
+
+traverse(root);
