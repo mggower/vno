15141c2d626b3e16356317b239273fbd4ee3620f
mggower <gowermikey@gmail.com>
2020-12-18 12:14:10 -0500

    run cli

---
+++ example/components/Purple.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="purple">
+  <h1>Kyle</h1>
+  <a href="https://imgbb.com/"
+    ><img src="https://i.ibb.co/6rxv4gC/kyle.png" alt="kyle" border="0"
+  /></a>
+  <p>Kyle (aka Grandpa Kyle) is here to party</p>
+</div>
+</template>
+
+<script>
+export default {
+name: 'purple',
+data() {
+  return {
+    color: 'purple',
+  };
+},
+};
+</script>
+
+<style>
+#purple {
+background-color: blueviolet;
+}
+</style>
