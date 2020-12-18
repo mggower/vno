ca00b7d74af6d1d2783287abecbb093c2235cafe
Jordan Grubb <imjordangrubb@gmail.com>
2020-12-18 13:16:18 -0500

    added closing div to orange

--- example/components/Orange.vue
+++ example/components/Orange.vue
@@ -3,6 +3,7 @@
   <h1>Jordan</h1>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/m46njhp/jordan.png" alt="jordan" border="0" /></a>
 <p>Jordan has worked on Broadway! So, yes, he has much sass!</p>
+</div>
 </template>
 
 <script>
