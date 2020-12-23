b2ac14612b3813fe6cb2b58248a49c4b9c54bbe1
mggower <gowermikey@gmail.com>
2020-12-23 14:23:42 -0500

    locating <style> is more flexible

--- demo-mikey/_1/App.vue
+++ demo-mikey/_1/App.vue
@@ -48,7 +48,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 body {
   width: 100vw;
   height: 100vh;
--- src/strategies/parser/utils/parseStyle.ts
+++ src/strategies/parser/utils/parseStyle.ts
@@ -4,20 +4,15 @@ import Utils from "../../../lib/utils.ts";
 const parseStyle = function pSt(current: ComponentInterface) {
   try {
     if (current.split) {
-      const { split } = current;
+      const open = Utils.indexOfRegExp(/<style.*>/gi, current.split);
+      const close = Utils.indexOfRegExp(/<\/style>/gi, current.split);
 
-      const open: number | undefined = split.indexOf("<style>");
-      const close: number | undefined = split.indexOf("</style>");
-
-      if (
-        (open < 0 || close < 0) ||
-        (typeof open !== "number" || typeof close !== "number")
-      ) {
+      if (open < 0 || close < 0) {
         current.style = undefined;
         return "parseStyle()=> succesful (no component styling)";
       }
 
-      current.style = Utils.sliceAndTrim(split, open + 1, close);
+      current.style = Utils.sliceAndTrim(current.split, open + 1, close);
 
       return "parseStyle()=> succesful";
     }
