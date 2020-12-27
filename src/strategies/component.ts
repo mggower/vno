a6e76edff92ad2167e052a5aa5c6183bfd4de3b0
mggower <gowermikey@gmail.com>
2020-12-27 14:50:45 -0500

    idk

--- src/strategies/component.ts
+++ src/strategies/component.ts
@@ -15,20 +15,27 @@ function Component(
 Component.prototype.runData = function data() {
   try {
     if (!this.path) {
-      throw `There was an error identifying the path for ${this.label}`;
+      throw (
+        `There was an error identifying the path for ${this.label}`
+      );
     }
 
     this.data = Deno.readTextFileSync(this.path);
 
     if (!this.data) {
-      throw `There was an error reading the file for path ${this.path}`;
+      throw (
+        `There was an error reading the file for path ${this.path}`
+      );
     }
 
     this.split = this.data.split(/\n/);
 
     return true;
   } catch (error) {
-    console.error("Error inside of Component.runData():", { error });
+    console.error(
+      "Error inside of Component.runData():",
+      { error },
+    );
   }
 };
 
