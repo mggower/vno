8f6c52020516c79f1b0400f3bf7694d5c2d0745a
mggower <gowermikey@gmail.com>
2020-12-30 19:18:40 -0500

    contains method added to SiblingList

--- src/strategies/sibling.ts
+++ src/strategies/sibling.ts
@@ -5,7 +5,7 @@ function SiblingList(this: SiblingInterface) {
   this.tail = null;
 }
 
-SiblingList.prototype.add = function (component: ComponentInterface) {
+SiblingList.prototype.add = function (component: object) {
   if (!this.head) {
     this.head = component;
     this.tail = component;
@@ -14,5 +14,28 @@ SiblingList.prototype.add = function (component: ComponentInterface) {
     this.tail = component;
   }
 };
+
+SiblingList.prototype.contains = function (label: string) {
+  if (!this.head) return false;
+  let current = this.head;
+  let prev;
+  while (current.sibling) {
+    console.log("current", current);
+    if (current.label === label) {
+      if (current === this.head) this.head = current.sibling;
+      if (prev) prev.sibling = current.sibling;
+      return true;
+    }
+    prev = current;
+    current = current.sibling;
+  }
+  if (current.label === label) {
+    this.tail = prev;
+    this.tail.sibling = null;
+    return true;
+  }
+  return false;
+};
+
 
 export default SiblingList;
