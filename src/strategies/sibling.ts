90e574bb48cb4cf800908fa444bf754a2c47e68a
mggower <gowermikey@gmail.com>
2020-12-31 22:24:12 -0500

    gitignore DS_Store

--- src/strategies/sibling.ts
+++ src/strategies/sibling.ts
@@ -17,25 +17,49 @@ SiblingList.prototype.add = function (component: object) {
 
 SiblingList.prototype.contains = function (label: string) {
   if (!this.head) return false;
-  let current = this.head;
+
+  if (this.head.label === label) {
+    if (this.head.sibling) {
+      this.head = this.head.sibling;
+    } else {
+      this.head = null;
+    }
+    return true;
+  }
+
+  let current;
   let prev;
+
+  if (this.head.sibling) {
+    current = this.head.sibling;
+    prev = this.head;
+  } else {
+    current = this.head;
+  }
+
   while (current.sibling) {
-    console.log("current", current);
     if (current.label === label) {
-      if (current === this.head) this.head = current.sibling;
-      if (prev) prev.sibling = current.sibling;
+      if (current === this.head) {
+        this.head = current.sibling;
+      }
+      if (prev) {
+        prev.sibling = current.sibling;
+      }
+
       return true;
     }
+
     prev = current;
     current = current.sibling;
   }
   if (current.label === label) {
     this.tail = prev;
     this.tail.sibling = null;
+
     return true;
   }
+
   return false;
 };
-
 
 export default SiblingList;
