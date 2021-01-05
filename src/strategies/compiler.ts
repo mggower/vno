9bad9d3ed492fe15969c6ae5256db14d963cbfe0
mggower <gowermikey@gmail.com>
2021-01-05 18:45:16 -0500

    main bundling cycle is commented

--- src/strategies/compiler.ts
+++ src/strategies/compiler.ts
@@ -3,20 +3,30 @@ import { CompilerInterface, ComponentInterface } from "../lib/types.ts";
 import { Storage } from "../lib/utils.ts";
 import _def from "../lib/defaults.ts";
 
+// #region Compiler
+// (3/3) in bundling cycle 
+// Compiler writes component instances to build file
+// and writes styling to style css file
+// #endregion
 function Compiler(this: CompilerInterface) {
+  // imports vue in build file
   this.vue = `import Vue from '${Storage.root.vue}';\n`;
+  // mounts the root component to the dom
   this.mount =
     `\n${Storage.root.label}.$mount("#${Storage.root.name}");\nexport default ${Storage.root.label};\n`;
 }
 
+// build establishes the build path and overwrites any previous builds
 Compiler.prototype.build = function () {
   try {
     fs.ensureDirSync(_def.VNO_PATH);
 
     if (fs.existsSync(_def.STYLE_PATH)) Deno.removeSync(_def.STYLE_PATH);
+    // write to build, lint ignore and import vue with CDN
     Deno.writeTextFileSync(_def.BUILD_PATH, _def.IGNORE + this.vue);
-
+    // invokes traverse to write the components in single threaded sequence
     this.traverse(Storage.root);
+    // write to build, mount app to the dom to complete
     Deno.writeTextFileSync(_def.BUILD_PATH, this.mount, { append: true });
   } catch (error) {
     return console.error(
@@ -25,7 +35,7 @@ Compiler.prototype.build = function () {
     );
   }
 };
-
+// write is responsible for writing each instance to the build
 Compiler.prototype.write = function w(current: ComponentInterface) {
   if (!current.instance) {
     throw (
@@ -38,6 +48,11 @@ Compiler.prototype.write = function w(current: ComponentInterface) {
   }
 };
 
+// #region traverse
+// traverse mimics a postorder tree traversal
+// it iterates through the component tree, and
+// writes from the least dependent to the root
+// #endregion
 Compiler.prototype.traverse = function trav(current: ComponentInterface) {
   if (current.child?.head) this.traverse(current.child.head);
   if (current.sibling) this.traverse(current.sibling);
