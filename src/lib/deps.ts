2376d3455b7be89b00ad3b1dd5414fb088dd704d
Erick Sosa <ericksosagarcias@gmail.com>
2021-01-09 20:39:40 -0400

    feat: support for scss in style tag

--- src/lib/deps.ts
+++ src/lib/deps.ts
@@ -8,9 +8,21 @@ import * as asrt from "https://deno.land/std@0.83.0/testing/asserts.ts";
 // third-party
 import _ from "https://cdn.skypack.dev/lodash"; // lodash
 import ProgressBar from "https://deno.land/x/progress@v1.2.3/mod.ts";
+import { compile as scssCompiler } from "https://raw.githubusercontent.com/crewdevio/deno_sass2/master/mod.ts";
 
 // oak
 import * as oak from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { superoak } from "https://deno.land/x/superoak@3.0.0/mod.ts";
 
-export { _, colors, fs, http, oak, path, ProgressBar, superoak, asrt };
+export {
+  _,
+  asrt,
+  colors,
+  fs,
+  http,
+  oak,
+  path,
+  ProgressBar,
+  scssCompiler,
+  superoak,
+};
--- src/strategies/parser-utils/parseStyle.ts
+++ src/strategies/parser-utils/parseStyle.ts
@@ -1,9 +1,11 @@
 import { ComponentInterface } from "../../lib/types.ts";
+import { scssCompiler } from "../../lib/deps.ts";
 import Utils from "../../lib/utils.ts";
 
 // parseStyle is responsible for parsing data inside of <style> tags
 export default function parseStyle(current: ComponentInterface) {
   try {
+    let useScss = false;
     if (current.split) {
       // isolate the content inside <style>
       const open = Utils.indexOfRegExp(/<style.*>/gi, current.split);
@@ -11,11 +13,26 @@ export default function parseStyle(current: ComponentInterface) {
       // return if the component has no added styling
       if (open < 0 || close < 0) {
         current.style = undefined;
-        return "parseStyle()=> succesful (no component styling)";
+        return "parseStyle()=> successful (no component styling)";
       }
       // stringify, trim, and save style to component object
       current.style = Utils.sliceAndTrim(current.split, open + 1, close);
       current.style = current.style.replace(Utils.multilineCommentPattern, "");
+
+      // detect scss style lang
+      for (const chunk of current.split) {
+        if (chunk.includes('lang="scss"')) {
+          useScss = true;
+        }
+      }
+
+      // stringify, trim, and save style to component object
+      current.style = Utils.sliceAndTrim(current.split, open + 1, close);
+
+      // compile scss to css
+      if (useScss) {
+        current.style = scssCompiler(current.style as string);
+      }
     }
   } catch (error) {
     console.error(
